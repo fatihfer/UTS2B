1274373ce4a25f54e82696bab2d019a834c04446
Fatihfer <fatihfernando01@gmail.com>
2023-05-11 19:49:31 +0700

    uts2b

---
+++ assets/scripts/js/harberweb.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $("#btnsearch").click(function(e){       
+        var txtInput = $("#srchInv").val();
+        console.log(txtInput);
+        $("#infoinvaccr").html("Nama Anda : " + txtInput);
+        $.ajax({
+                type:"POST",
+                url:"/assets/scripts/ajax/getcurl.php",
+                data : "",
+                success : function (response){
+                    $("#infoinvaccr").html("<br/>Response<br/>" + response);
+                }
+            });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// $.ajax({
+        //      type: "POST",
+        //      url: "/assets/scripts/ajax/getcurl.php",
+        //      data: "",
+        //      success: function (response) {
+        //          $("#infoinvaccr").html("Response<br/> " + response) ;
+        //      }
+        //})
